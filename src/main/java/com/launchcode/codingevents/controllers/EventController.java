fbd87e877d0bafeeeff2f7917f52c3d14537ee41
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-05-06 19:45:34 -0500

    Chapter 14 - Added delete event method

--- src/main/java/com/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/com/launchcode/codingevents/controllers/EventController.java
@@ -15,17 +15,37 @@ public class EventController {
     
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title","Coding Events");
         model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
     
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("title","Create Event");
         return "events/create";
     }
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDesc){
+    public String processCreateEventForm(@RequestParam String eventName,
+                                         @RequestParam String eventDesc){
         EventData.add(new Event(eventName, eventDesc));
+        return "redirect:/events";
+    }
+    
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title","Delete Events");
+        model.addAttribute("events",EventData.getAll());
+        return "events/delete";
+    }
+    
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds){
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:/events";
     }
 }
